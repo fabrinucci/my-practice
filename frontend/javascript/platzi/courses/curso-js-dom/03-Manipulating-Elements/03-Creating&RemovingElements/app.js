f6344817d9fa53ad2bd1c29e44f47b1496e36ae2
Fabrizio <fabrinucci99@gmail.com>
2024-09-01 16:24:12 -0300

    add curso-js-dom

---
+++ frontend/javascript/platzi/courses/curso-js-dom/03-Manipulating-Elements/03-Creating&RemovingElements/app.js
@@ -0,0 +1,4 @@
+// const contentArea = document.getElementById('listArea');
+// const newElement = document.createElement('li');
+// newElement.textContent = 'New item 5';
+// contentArea.append(newElement);
